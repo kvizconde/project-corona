80c9ceb9dba12f36244c065136f15599df4f34f8
Kevin Vizconde <kcarlv@gmail.com>
2020-04-22 12:55:40 -0700

    added a condition in extractCountry function that handles undefined

--- static/js/index.js
+++ static/js/index.js
@@ -21,24 +21,26 @@ function extractCountry(country = 'World', flag = '🌎 ') {
   const recovered = document.getElementById('recovered');
   const deaths = document.getElementById('deaths');
 
-  cases.textContent =
-    country.cases !== null
-      ? `${flag} ${country.cases.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}`
-      : 'N/A 😔';
-  recovered.textContent =
-    country.recovered !== null
-      ? country.recovered.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
-      : 'N/A 😔';
-  deaths.textContent =
-    country.deaths !== null
-      ? country.deaths.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
-      : 'N/A 😔';
+  if (country.cases) {
+    cases.textContent =
+      country.cases !== null
+        ? `${flag} ${country.cases.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}`
+        : 'N/A 😔';
+    recovered.textContent =
+      country.recovered !== null
+        ? country.recovered.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+        : 'N/A 😔';
+    deaths.textContent =
+      country.deaths !== null
+        ? country.deaths.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+        : 'N/A 😔';
 
-  const rPercent = document.getElementById('recoverPercent');
-  const dPercent = document.getElementById('deathPercent');
+    const rPercent = document.getElementById('recoverPercent');
+    const dPercent = document.getElementById('deathPercent');
 
-  rPercent.textContent = `${((country.recovered / country.cases) * 100).toFixed(2)} %`;
-  dPercent.textContent = `${((country.deaths / country.cases) * 100).toFixed(2)} %`;
+    rPercent.textContent = `${((country.recovered / country.cases) * 100).toFixed(2)} %`;
+    dPercent.textContent = `${((country.deaths / country.cases) * 100).toFixed(2)} %`;
+  }
 }
 
 const inputTouch = document.querySelectorAll('.onTouch');
